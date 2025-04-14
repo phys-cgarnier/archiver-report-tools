a657287f411c482c30df78ecb05135245e6cb343
chris garnier <cgarnier@slac.stanford.edu>
2025-04-14 15:13:58 -0700

    fixed typo

--- report_tool.py
+++ report_tool.py
@@ -74,7 +74,7 @@ class ArchiverUtility:
             if pv in filtered and filtered[pv] != {}:
                 if disconnected_status or filters['status'] is None:
                     pv_connection = epics.PV(pv)
-                    if not pv.wait_for_connection(timeout=2.5):
+                    if not pv_connection.wait_for_connection(timeout=2.5):
                         filtered[pv]['connected pv'] = pv_connection.connected
                 report.update(filtered)
 
